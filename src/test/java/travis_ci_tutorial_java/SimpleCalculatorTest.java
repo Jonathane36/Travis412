e3427187bd171d3627443704b5d9918abef95a6e
jeldri2 <jeldri2@students.towson.edu>
2023-12-14 06:05:01 -0500

    Calc updated

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,15 +11,17 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	@Test
 	public void testMinus(){
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.minus(5, 3), 2);
 	}
-	
+	@Test
 	public void testMultiply(){
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.multiply(4, 5), 12);
+		assertEquals(calc.multiply(4, 5), 20);
 	}
+	@Test
 	public void testDivide(){
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.divide(10, 5), 2);
